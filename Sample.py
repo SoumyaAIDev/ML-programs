c4a87b4d0216bc2e6bf5e362e60e15bd55210b48
Soumya D <104708894+SoumyaAIDev@users.noreply.github.com>
2024-09-18 23:26:37 +0530

    Sample.py

---
+++ Sample.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+
+
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import Randomizedsearcher
+from sklearn.metrics import accuracy_score,classification_report
